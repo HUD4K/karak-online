20da9e80b37801c6506b69b09ba81e5ae5e28729
HUD4K <mh.michalhudak@gmail.com>
2024-01-12 00:42:29 +0100

    updated character positions

--- src/karakGameFlow3DScene.js
+++ src/karakGameFlow3DScene.js
@@ -90,11 +90,11 @@ class KarakGameFlow {
 
     // Vaša existujúca logika pre pridanie hráčov a charakterov
     const offsets = [
-        { x: -0.4, y: 0.4 },
-        { x: 0.4, y: -0.4 },
-        { x: 0.4, y: 0.4 },
-        { x: -0.4, y: -0.4 },
-        { x: 0.4, y: 0.2 }
+        { x: -0.3, y: 0.3 },
+        { x: 0.3, y: -0.3 },
+        { x: 0.3, y: 0.3 },
+        { x: -0.3, y: -0.3 },
+        { x: 0.3, y: 0.1 }
     ];
 
     sampleCharacters.forEach((character, index) => {
